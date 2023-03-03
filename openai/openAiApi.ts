dad09e5d7d1bbf26a10257235445b762047f9ece
Sebastian Schalow <schalowseb@gmail.com>
2023-03-03 19:15:12 +0100

    Remove unused imports

--- openai/openAiApi.ts
+++ openai/openAiApi.ts
@@ -1,5 +1,4 @@
 import {Configuration, OpenAIApi} from "openai";
-import {CreateCompletionResponseChoicesInner} from "openai/api";
 
 const configuration = new Configuration({
     organization: process.env.OPEN_AI_ORG,
